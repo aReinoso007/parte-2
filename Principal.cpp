b89305921f38e0f9376d6256d47757268d67e217
Alex Reinoso <ralexjessiel@gmail.com>
2020-11-23 21:31:06 -0500

    inicio

---
+++ Principal.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstdlib>
+#include <cmath>
+#include <vector>
+
+#include <opencv4/opencv2/core/core.hpp>
+#include <opencv4/opencv2/highgui/highgui.hpp>
+#include <opencv4/opencv2/imgproc/imgproc.hpp>
+#include <opencv4/opencv2/video/video.hpp>
+#include <opencv4/opencv2/videoio/videoio.hpp>
+#include <opencv4/opencv2/imgcodecs/imgcodecs.hpp>
+
+using namespace std; 
+using namespace cv;
