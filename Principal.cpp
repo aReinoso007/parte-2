14ca7139acf1dd2010de2b9063b716572f3f8372
Alex Reinoso <ralexjessiel@gmail.com>
2020-11-24 12:49:17 -0500

    leer directorio

--- Principal.cpp
+++ Principal.cpp
@@ -2,6 +2,9 @@
 #include <cstdlib>
 #include <cmath>
 #include <vector>
+#include <filesystem>
+
+
 
 #include <opencv4/opencv2/core/core.hpp>
 #include <opencv4/opencv2/highgui/highgui.hpp>
@@ -13,6 +16,17 @@
 using namespace std; 
 using namespace cv;
 
+
 int main(int argc, char *argv[]){
     
+    vector<string> fileName; /* */
+    string path = "/home/alex/Documents/8vo/vision-artificial/practicas/parte-2/Icons-50/airplane";
+    cv::glob(path, fileName, false);
+    cout << fileName.size() << endl;
+    for(int i =0; i < fileName.size(); i++){
+        cout << fileName[i] << endl;
+    }
+    
+    return 0;
+
 }
